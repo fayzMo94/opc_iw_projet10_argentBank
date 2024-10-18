960c8798cdc1c868f0b968efde3b748f1d4f12bb
Xart94 <fayz_mohamed2@hotmail.fr>
2024-10-18 11:56:15 +0200

    Banner and Login Page integration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Header />
+      {/* <Home/> */}
+      <LoginPage />
+      <Footer />
     </div>
   );
 }
