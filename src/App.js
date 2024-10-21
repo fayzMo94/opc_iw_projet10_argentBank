53a1f7930aec4cce47fe9c4a847f2a7495cdcb71
Xart94 <fayz_mohamed2@hotmail.fr>
2024-10-21 10:36:24 +0200

    router added & css adjustments

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import { Header } from "./components/Header/Header";
 import { User } from "./components/User/User";
 import Home from "./pages/Home/Home";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
-import {UserProfile} from "./pages/UserProfile/UserProfile.jsx";
+import { UserProfile } from "./pages/UserProfile/UserProfile.jsx";
+import { Route, Routes } from "react-router-dom";
+import { ErrorPage } from "./pages/Error/ErrorPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home/>
-      {/* <LoginPage /> */}
-      {/* <UserProfile /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="user" element={<UserProfile />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
       <Footer />
     </div>
   );
