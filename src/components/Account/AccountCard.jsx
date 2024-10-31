7695d904111d287974beb3a14b618f31e2695169
Xart94 <fayz_mohamed2@hotmail.fr>
2024-10-31 12:49:08 +0100

    account & transactions integration

--- src/components/Account/AccountCard.jsx
+++ src/components/Account/AccountCard.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import './AccountCard.css'
+import React, { useState } from "react";
+import "./AccountCard.css";
+import Transactions from "../Transactions/Transactions";
 
-export const AccountCard = ({data}) => {
+export const AccountCard = ({ data }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <section className="account">
-      <div className="account-content-wrapper">
-        <h3 className="account-title">{data.title}</h3>
-        <p className="account-amount">{data.amount}</p>
-        <p className="account-amount-description">{data.description}</p>
-      </div>
-      <div className="account-content-wrapper cta">
-        <button className="transaction-button">View transactions</button>
-      </div>
-    </section>
+    <div className="accountCard">
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">{data.title}</h3>
+          <p className="account-amount">{data.amount}</p>
+          <p className="account-amount-description">{data.description}</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button
+            onClick={() => setCollapsed(!collapsed)}
+            className="transaction-button"
+          >
+            View transactions
+          </button>
+        </div>
+      </section>
+      {collapsed ? <Transactions /> : null}
+    </div>
   );
-}
+};
